2144338b3268bcb8373dc51850014c282e9d4762
koushikamboru <amborukoushik@gmail.com>
2024-11-28 12:12:07 +0530

    spring

---
+++ bhavaspace/src/main/java/com/example/bhavaspace/BhavaspaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bhavaspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhavaspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhavaspaceApplication.class, args);
+	}
+
+}
---
+++ bhavaspace/src/test/java/com/example/bhavaspace/BhavaspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bhavaspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhavaspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
